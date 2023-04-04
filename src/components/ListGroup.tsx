20012da527b9c458960559d4d3406f7e1ae873f3
StevenVallarsa <orionsjv@gmail.com>
2023-04-03 23:06:21 -0400

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,9 @@
 export default function ListGroup() {
-  const cities = ["Sudbury", "Toronto", "Ottawa", "North Bay", "Sarnia"];
+  const cities: any[] = ["Sudbury", "Toronto", "Ottawa", "North Bay", "Sarnia"];
   return (
     <>
       <h1>LIST</h1>
+      {cities.length === 0 && <p>No Cities Found</p>}
       <ul className="list-group">
         {cities.map((city, index) => (
           <li key={index} className="list-group-item">
