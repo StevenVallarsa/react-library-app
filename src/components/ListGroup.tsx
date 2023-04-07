ad1cb3d9d83f4631bdb693b247fed710e9fe72c3
StevenVallarsa <orionsjv@gmail.com>
2023-04-06 21:49:37 -0400

    move method out of onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,13 @@
 export default function ListGroup() {
   const cities: any[] = ["Sudbury", "Toronto", "Ottawa", "North Bay", "Sarnia"];
+  const handleClick = (city: string) => console.log(city);
   return (
     <>
       <h1>LIST</h1>
       {cities.length === 0 && <p>No Cities Found</p>}
       <ul className="list-group">
         {cities.map((city, index) => (
-          <li key={index} className="list-group-item">
+          <li key={index} onClick={() => handleClick(city)} className="list-group-item">
             {city}
           </li>
         ))}
