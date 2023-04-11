6068236be400e82d7d9c926dbee52a6dc7683717
StevenVallarsa <orionsjv@gmail.com>
2023-04-10 22:40:37 -0400

    add state of clicked item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 export default function ListGroup() {
   const cities: any[] = ["Sudbury", "Toronto", "Ottawa", "North Bay", "Sarnia"];
-  const handleClick = (city: string) => console.log(city);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const handleClick = (index: number) => setSelectedIndex(index);
+
   return (
     <>
       <h1>LIST</h1>
       {cities.length === 0 && <p>No Cities Found</p>}
       <ul className="list-group">
         {cities.map((city, index) => (
-          <li key={index} onClick={() => handleClick(city)} className="list-group-item">
+          <li
+            key={index}
+            onClick={() => handleClick(index)}
+            className={`list-group-item ${selectedIndex === index ? "active" : ""}`}
+          >
             {city}
           </li>
         ))}
@@ -15,3 +23,5 @@ export default function ListGroup() {
     </>
   );
 }
+
+// npm run dev
