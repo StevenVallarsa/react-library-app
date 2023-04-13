070d53adbeb142158caa87c73be666c0b9c9335a
StevenVallarsa <orionsjv@gmail.com>
2023-04-12 22:50:15 -0400

    pass function as prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import ListGroup from "./components/ListGroup";
 
 const cities: any[] = ["Sudbury", "Toronto", "Ottawa", "North Bay", "Sarnia"];
 
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
+
 function App() {
   return (
     <div>
-      <ListGroup items={cities} heading="cities" />
+      <ListGroup items={cities} heading="Cities" onSelectItem={handleSelectItem} />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,19 +3,23 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-export default function ListGroup({ items, heading }: Props) {
+export default function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const handleClick = (index: number) => {
     index === selectedIndex ? setSelectedIndex(-1) : setSelectedIndex(index);
+    onSelectItem(items[index]);
   };
 
   return (
     <>
       <h1>{heading.toUpperCase()}</h1>
-      {items.length === 0 && <p>No {heading.substring(0, 1).toUpperCase() + heading.substring(1)} Found</p>}
+      {items.length === 0 && (
+        <p>No {heading.substring(0, 1).toUpperCase() + heading.substring(1).toLowerCase()} Found</p>
+      )}
       <ul className="list-group">
         {items.map((city, index) => (
           <li
